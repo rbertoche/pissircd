f561c3c4f0cfe87f97a309c356a04e28439aec64
Ramiro Bou <bouramiro@gmail.com>
2021-07-16 13:08:59 -0300

    G-Peeing

--- src/modules/third/noglobalmsg.c
+++ src/modules/third/noglobalmsg.c
@@ -2,10 +2,22 @@
  *   IRC - Internet Relay Chat, src/modules/third/noglobalmsg.c
  *   (C) 2021 Polsaker - All rights reserved.
  *
- *   You are granted unlimited and unrestricted rights to use, modify 
- *   and redistribute the licensed work, pursuant to the following terms:
- *   1. You must piss
- *   2. It must be more than 100ml.
+ *   See file AUTHORS in IRC package for additional names of
+ *   the programmers.
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 1, or (at your option)
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
 #include "unrealircd.h"
