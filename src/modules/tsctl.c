unrealircd/unrealircd

Senting mtags on TSCTL ALLTIME

src/modules/tsctl.c
@@ -66,7 +66,7 @@ CMD_FUNC(cmd_tsctl)
 	{
 		sendnotice(client, "*** Server=%s TStime=%lld",
 			me.name, (long long)TStime());
-		sendto_server(client, 0, 0, NULL, ":%s TSCTL alltime", client->id);
+		sendto_server(client, 0, 0, recv_mtags, ":%s TSCTL alltime", client->id);
 		return;
 	}
 }

rbertoche/pissircd

src/modules/tsctl.c
@@ -68,7 +68,7 @@ CMD_FUNC(cmd_tsctl)
 		gettimeofday(&currenttime_tv, NULL);
 		sendnotice(client, "*** Server=%s TStime=%lld.%ld",
 			me.name, (long long)currenttime_tv.tv_sec, (long)currenttime_tv.tv_usec);
-		sendto_server(client, 0, 0, NULL, ":%s TSCTL alltime", client->id);
+		sendto_server(client, 0, 0, mtags, ":%s TSCTL alltime", client->id);
 		return;
 	}
 }